95bfa7bee475e152d8d2aa9dccdc5ef9d5860f8d
Devaanshraheja <148315570+Devaanshraheja@users.noreply.github.com>
2024-12-28 19:19:08 +0530

    commit

---
+++ 01problem.py
@@ -0,0 +1,9 @@
+
+
+
+a=[12,"deva",3232,"    "]
+print(a[2])
+print(a)
+a[2]="ananya"# unlike  strings list are mutable you can change the original list 
+
+print(a*3)
---
+++ 03_problem.py
@@ -0,0 +1,12 @@
+fruit=[]
+f1=input("Enter the fruit name:")
+fruit.append(f1)
+f2=input("Enter the fruit name:")
+fruit.append(f2)
+f3=input("Enter the fruit name:")
+fruit.append(f3)
+f4=input("Enter the fruit name:")
+fruit.append(f4)
+f5=input("Enter the fruit name:")
+fruit.append(f5)
+print(fruit)
